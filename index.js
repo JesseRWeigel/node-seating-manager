920b7d8d704d13fa7bdacd92fd7f2135b024a53a
Jesse Weigel <jesseweigel@gmail.com>
2018-02-07 15:43:45 -0500

    feat: add initial user input prompts

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer')
+
+// This holds the data for seating
+const seatingChart = []
+
+// Initial questions posed to user go here
+const questions = [
+  {
+    type: 'input',
+    name: 'rows',
+    message: "How many rows of seats to you have?"
+  },
+  {
+    type: 'input',
+    name: 'seatsPerRow',
+    message: "How many seats in each row?",
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  //Reply to user with total number of rows, seats per row, and seats.
+  const outputStr = `You have ${answers.rows} rows and ${answers.seatsPerRow} seats per row for a total of ${answers.rows * answers.seatsPerRow} seats.`
+
+  console.log(outputStr);
+})
