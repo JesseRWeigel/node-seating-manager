d53b16304f9d7c86cf87769c71b8caf4321e38f2
Jesse Weigel <jesseweigel@gmail.com>
2018-02-10 14:57:02 -0500

    feat: create array of arrays to represent seating chart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer')
 
 // This holds the data for seating
-const seatingChart = []
+let seatingChart
 
 // Initial questions posed to user go here
 const questions = [
@@ -19,7 +19,18 @@ const questions = [
 
 inquirer.prompt(questions).then(answers => {
   // Reply to user with total number of rows, seats per row, and seats.
-  const outputStr = `You have ${answers.rows} rows and ${answers.seatsPerRow} seats per row for a total of ${answers.rows * answers.seatsPerRow} seats.`
+  const outputStr = `You have ${answers.rows} rows and ${
+    answers.seatsPerRow
+  } seats per row for a total of ${answers.rows * answers.seatsPerRow} seats.`
 
+  // Create two dimensional array to represent seating
+  seatingChart = new Array(answers.rows)
+  for (let i = 0; i < answers.rows; i++) {
+    seatingChart[i] = new Array(answers.seatsPerRow)
+    for (let j = 0; j < answers.seatsPerRow; j++) {
+      seatingChart[i][j] = 0
+    }
+  }
+  console.log(seatingChart)
   console.log(outputStr)
 })
